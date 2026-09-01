1f961b6f748844be0890a821e0b6700819511df4
Prachi <royprachi2501@gmail.com>
2026-09-02 00:28:09 +0530

    Added star pattern program

--- Pattern8.cpp
+++ Pattern8.cpp
@@ -4,19 +4,18 @@ int main(){
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        //space 
-        for(int j=0;j<n-i-1;j++){
+        //space
+        for(int j=0;j<i;j++){
             cout<<" ";
         }
         //stars
-        for(int j=0;j<2*i+1;j++){
+        for(int j=2*(n-i)-1;j>=1;j--){
             cout<<"*";
         }
-         for(int j=0;j<n-i-1;j++){
+        //space
+        for(int j=0;j<i;j++){
             cout<<" ";
         }
-
-        cout<<endl;
-
+        cout<<endl; 
     }
 }
