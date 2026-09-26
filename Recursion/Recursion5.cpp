2aa2eef10fb3282ba52bc1438cc7ac1d8790426b
Prachi <royprachi2501@gmail.com>
2026-09-26 22:57:54 +0530

    Recursion problem

--- Recursion/Recursion5.cpp
+++ Recursion/Recursion5.cpp
@@ -2,14 +2,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 void recursion(int i,int n){
-    if(n>i){
+    if(n<i){
         return;
     }
-    recursion(i-1,n);
+    recursion(i+1,n);
     cout<<i<<endl;
 }
 int main(){
     int n;
     cin>>n;
-    recursion(n,n);
+    recursion(1,n);
 }
