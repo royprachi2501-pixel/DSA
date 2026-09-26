fb56e2ed64c43ebe7da2cb891f6d03b0af3a6833
Prachi <royprachi2501@gmail.com>
2026-09-26 21:44:10 +0530

    recursion using backtracking

--- Recursion/Recursion5.cpp
+++ Recursion/Recursion5.cpp
@@ -1,15 +1,15 @@
-//printing linearly decreasingnumbers using backtrack recursion
+//printing linearly decreasing numbers using backtrack recursion
 #include<bits/stdc++.h>
 using namespace std;
 void recursion(int i,int n){
-    if(n<i){
+    if(n>i){
         return;
     }
-    recursion(i+1,n);
+    recursion(i-1,n);
     cout<<i<<endl;
 }
 int main(){
     int n;
     cin>>n;
-    recursion(1,n);
+    recursion(n,n);
 }
